13ea0cf0c4cbe0af1810e06343fd1a4e4f023a90
Alex Orekhov <everget@users.noreply.github.com>
2017-04-06 19:16:13 +0300

    Create typeOf.js

---
+++ typeOf.js
@@ -0,0 +1,13 @@
+function typeOf(value) {
+  let result;
+
+  result = getTag(value);
+
+  return result || void 0;
+
+  function getTag(value) {
+    return ({}).toString.call(value)
+      .match(/\s([a-zA-Z]+(\s[a-zA-Z]+)*)/)[1]
+      .toLowerCase();
+  }
+}
