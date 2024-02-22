2ab730554299145b28a19e392a686cba3796e387
Alex @everget <alex.everget161@gmail.com>
2024-02-23 00:19:29 +0300

    ci: fix coverage artifacts upload/download

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 	test: {
 		coverage: {
 			provider: 'v8',
-			reporter: ['html', 'clover', 'json', 'lcov'],
+			reporter: ['json', 'lcov'],
 			exclude: [
 				'**/{husky,lint-staged}.config.*',
 				'**/.{eslint,prettier,remark}rc.{?(c|m)js,yml}',
