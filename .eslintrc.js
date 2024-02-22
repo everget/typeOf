10a750eb4f0aceacdd8be187dad5d336390a889f
Alex @everget <alex.everget161@gmail.com>
2024-02-22 23:06:27 +0300

    chore: cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,10 @@
 module.exports = {
 	root: true,
-
 	env: {
 		browser: true,
 		node: true,
 		es6: true,
-		mocha: true,
 	},
-	ignorePatterns: ['!src'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
