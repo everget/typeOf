e780015b83890ed14a5a2b89edad770466ca2faa
Alex @everget <alex.everget161@gmail.com>
2024-02-22 18:57:24 +0300

    ci: fix ci workflow and coverage generation

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,11 @@ export default defineConfig({
 	test: {
 		coverage: {
 			provider: 'v8',
-			reporter: ['text', 'html', 'clover', 'json'],
+			reporter: ['html', 'clover', 'json', 'lcov'],
+			exclude: [
+				'**/{husky,lint-staged}.config.*',
+				'**/.{eslint,prettier,remark}rc.{?(c|m)js,yml}',
+			],
 		},
 	},
 });
